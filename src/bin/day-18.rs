4e3b7ebd30821f4e94fcd005ce47d09cc4411e13
Nathaniel Verhaaren <nverhaaren94@gmail.com>
2023-12-18 00:31:26 -0600

    18b passing

--- src/bin/day-18.rs
+++ src/bin/day-18.rs
@@ -1,6 +1,5 @@
 use std::{io, str};
 use std::io::{BufRead, BufReader};
-use std::str::FromStr;
 use itertools::Itertools;
 use regex::Regex;
 use aoc_2023::coordinate::{Direction, ICoordinate, twice_shoelace};
@@ -8,16 +7,17 @@ use aoc_2023::coordinate::{Direction, ICoordinate, twice_shoelace};
 fn main() {
     let stdin = io::stdin();
     let reader = BufReader::with_capacity(256, stdin.lock());
-    let plans: Vec<_> = reader.lines()
-        .map(|r| r.map_err(|_| ()).and_then(|line| -> Result<Plan, _> { line.parse() } ))
+    let lines: Vec<_> = reader.lines()
         .try_collect().expect("Unicode issue");
-    println!("First part: {}", part_1(&plans));
-    println!("Second part: {}", part_2(&plans));
+    let plans_1: Vec<Plan> = lines.iter().map(|line| Plan::parse_1(line)).collect();
+    let plans_2: Vec<Plan> = lines.iter().map(|line| Plan::parse_2(line)).collect();
+    println!("First part: {}", compute_covered(&plans_1));
+    println!("Second part: {}", compute_covered(&plans_2));
 }
 
 type Point = ICoordinate<2>;
 
-fn part_1(plans: &[Plan]) -> usize {
+fn compute_covered(plans: &[Plan]) -> usize {
     let boundary: usize = plans.iter().map(|plan| plan.length).sum();
     let mut current = Point::origin();
     let twice_area = twice_shoelace(plans.iter()
@@ -34,22 +34,16 @@ fn part_1(plans: &[Plan]) -> usize {
     (diff / 2 + 1) + boundary
 }
 
-fn part_2(plans: &[Plan]) -> usize {
-    todo!()
-}
-
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 struct Plan {
     direction: Direction,
     length: usize,
-    rgb: (u8, u8, u8),
 }
 
-impl FromStr for Plan {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let re = Regex::new(r"^([RLUD]) (\d+) \(#([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})\)$").unwrap();
-        let (_, [dir, len, r, g, b]) = re.captures(s).ok_or(())?.extract();
+impl Plan {
+    pub fn parse_1(s: &str) -> Self {
+        let re = Regex::new(r"^([RLUD]) (\d+) \(#[0-9a-f]{6}\)$").unwrap();
+        let (_, [dir, len]) = re.captures(s).expect("input issue").extract();
         let direction = match dir {
             "R" => Direction::East,
             "L" => Direction::West,
@@ -57,14 +51,26 @@ impl FromStr for Plan {
             "D" => Direction::South,
             _ => unreachable!(),
         };
-        let length: usize = len.parse().map_err(|_| ())?;
-        let red = u8::from_str_radix(r, 16).map_err(|_| ())?;
-        let green = u8::from_str_radix(g, 16).map_err(|_| ())?;
-        let blue = u8::from_str_radix(b, 16).map_err(|_| ())?;
-        Ok(Self { direction, length, rgb: (red, green, blue) })
+        let length: usize = len.parse().unwrap();
+
+        Self { direction, length }
+    }
+
+    pub fn parse_2(s: &str) -> Self {
+        let re = Regex::new(r"^[RLUD] \d+ \(#([0-9a-f]{5})([0-9a-f])\)$").unwrap();
+        let (_, [len, dir]) = re.captures(s).expect("input issue").extract();
+        let direction = match dir {
+            "0" => Direction::East,
+            "2" => Direction::West,
+            "3" => Direction::North,
+            "1" => Direction::South,
+            _ => unreachable!(),
+        };
+        let length: usize = usize::from_str_radix(len, 16).unwrap();
+
+        Self { direction, length }
     }
 }
-
 #[cfg(test)]
 mod test {
     use super::*;
