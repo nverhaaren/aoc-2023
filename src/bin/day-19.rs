a65ef5e139ec69c51e09193983bfd64570a3ed93
Nathaniel Verhaaren <nverhaaren94@gmail.com>
2023-12-19 20:05:32 -0600

    19a passing

--- src/bin/day-19.rs
+++ src/bin/day-19.rs
@@ -1,4 +1,5 @@
 use std::{io, str};
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 use std::ops::Index;
 use std::str::FromStr;
@@ -11,8 +12,32 @@ fn main() {
     let reader = BufReader::with_capacity(256, stdin.lock());
     let lines: Vec<_> = reader.lines()
         .try_collect().expect("Unicode issue");
-    // println!("First part: {}", compute_covered(&plans_1));
+    let mut lines_iter = lines.iter();
+    let parser = Parser::new();
+    let map = parse_rules((&mut lines_iter).map(|s| s.as_str()), &parser)
+        .collect();
+    println!(
+        "First part: {}",
+        part_1(&map, parse_parts(lines_iter.map(|s| s.as_str()), &parser))
+    );
     // println!("Second part: {}", compute_covered(&plans_2));
+}
+
+fn part_1(map: &HashMap<String, Rule>, parts: impl Iterator<Item=Part>) -> u64 {
+    parts
+        .filter(|part| {
+            let mut next = "in";
+            loop {
+                let rule = map.get(next).expect("map issue");
+                match rule.destination(part) {
+                    Destination::Accept => break true,
+                    Destination::Reject => break false,
+                    Destination::Rule(s) => next = s.as_str(),
+                }
+            }
+        })
+        .map(|part| part.total_rating())
+        .sum()
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
@@ -21,6 +46,12 @@ struct Part {
     m: u64,
     a: u64,
     s: u64,
+}
+
+impl Part {
+    pub fn total_rating(&self) -> u64 {
+        self.x + self.m + self.a + self.s
+    }
 }
 
 impl Index<Field> for Part {
@@ -117,7 +148,7 @@ impl Parser {
         Part { x: parse_number(x), m: parse_number(m), a: parse_number(a), s: parse_number(s) }
     }
 
-    pub fn guard_dest_re(&self, s: &str) -> Result<(Guard, Destination), Destination> {
+    pub fn parse_rule_entry(&self, s: &str) -> Result<(Guard, Destination), Destination> {
         let (_, [field, comp, value, dest]) = self.guard_dest_re.captures(s)
             .ok_or_else(|| s.parse().unwrap())?.extract();
         let field = match field {
@@ -132,12 +163,66 @@ impl Parser {
             dest.parse().unwrap()
         ))
     }
+
+    pub fn parse_rule(&self, s: &str) -> (String, Rule) {
+        let (before, after) = s.split_once('{').expect("Invalid rule");
+        let (after, empty) = after.split_once('}').expect("Invalid rule");
+        assert!(empty.is_empty());
+        let mut chain = vec![];
+        for entry in after.split(',') {
+            match self.parse_rule_entry(entry) {
+                Ok(parsed) => chain.push(parsed),
+                Err(terminal) => {
+                    return (before.to_owned(), Rule { chain, terminal })
+                }
+            }
+        };
+        unreachable!();
+    }
 }
 
-// todo: full line parsing (rules then parts)
+fn parse_rules<'a>(
+    it: impl Iterator<Item=&'a str> + 'a,
+    parser: &'a Parser
+) -> impl Iterator<Item=(String, Rule)> + 'a {
+    it.take_while(|s| !s.is_empty())
+        .map(move |s| parser.parse_rule(s))
+}
+
+fn parse_parts<'a>(
+    it: impl Iterator<Item=&'a str> + 'a,
+    parser: &'a Parser
+) -> impl Iterator<Item=Part> + 'a {
+    it.take_while(|s| !s.is_empty())
+        .map(move |s| parser.parse_part(s))
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[test]
+    fn test_parse_rule() {
+        let expected = ("px".to_owned(), Rule { chain: vec![
+            (
+                Guard { field: Field::A, less_than: true, value: 2006 },
+                Destination::Rule("qkq".to_owned())
+            ),
+            (
+                Guard { field: Field::M, less_than: false, value: 2090 },
+                Destination::Accept
+            )
+        ], terminal: Destination::Rule("rfg".to_owned()) });
+        let parser = Parser::new();
+
+        assert_eq!(expected, parser.parse_rule("px{a<2006:qkq,m>2090:A,rfg}"));
+    }
+
+    #[test]
+    fn test_parse_part() {
+        let part = Part { x: 787, m: 2655, a: 1222, s: 2876 };
+        let parser = Parser::new();
+
+        assert_eq!(part, parser.parse_part("{x=787,m=2655,a=1222,s=2876}"));
+    }
 }
